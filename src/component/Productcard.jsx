d5f59a023e86b685681bfe2e5411ef24311eb756
leetxt13 <125445443+leetxt13@users.noreply.github.com>
2025-02-20 13:30:37 +0900

    grid modified

--- src/component/Productcard.jsx
+++ src/component/Productcard.jsx
@@ -5,12 +5,16 @@ export default function Productcard({
 }) {
   return (
     <li className='rounded-lg shadow-md overflow-hidden cursor-pointer border border-slate-800'>
-      <img className='bg-slate-200 w-full md:h-72 ' src={image} alt={title} />
+      <img
+        className='bg-slate-200 w-full h-40 md:h-72 '
+        src={image}
+        alt={title}
+      />
       <div className='mt-2 px-2 text-lg flex justify-between items-center'>
-        <h3 className='truncate'>{title}</h3>
-        <p>{`${price}원`}</p>
+        <h3 className='truncate text-xs md:text-base'>{title}</h3>
+        <p className='text-xs md:text-base'>{`${price}원`}</p>
       </div>
-      <p className='mb-2 px-2 text-gray-600'>{category}</p>
+      <p className='mb-2 px-2 text-gray-600 text-xs md:text-base'>{category}</p>
     </li>
   );
 }
