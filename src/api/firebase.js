db5fc48efa74aff03a8ab19afc43b71e81cd4d8c
leetxt13 <125445443+leetxt13@users.noreply.github.com>
2025-02-23 05:30:49 +0900

    popup

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -26,8 +26,8 @@ provider.setCustomParameters({ prompt: 'select_account' });
 //-> 계정선택화면이 안나오는 오류때문에 사용
 const database = getDatabase(app);
 export async function login() {
-  // signInWithPopup(auth, provider).catch(console.error);
-  signInWithRedirect(auth, provider).catch(console.error);
+  signInWithPopup(auth, provider).catch(console.error);
+  // signInWithRedirect(auth, provider).catch(console.error);
 }
 
 export async function logout() {
